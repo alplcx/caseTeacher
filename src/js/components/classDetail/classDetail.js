cf6dd1fbf88212846ee3aab94d59311765f9be09
zhangzhang <1663443357@qq.com>
2016-08-21 17:35:02 +0800

    更新

--- src/js/components/classDetail/classDetail.js
+++ src/js/components/classDetail/classDetail.js
@@ -3,16 +3,39 @@
 
 var BaseComponet = require('../../common/component.js'); 
 var template     = require('./classDetail.html');
-var _            = require('../../common/util.js')
+var _            = require('../../common/util.js');
+var Service      = require('../../service.js');
 
 var ClassDetail = BaseComponet.extend({
     name : "classDetail",     
-	template:template,   
+	template:template, 
+	service:Service,  
 	config:function(data){  
-	   
+	   _.extend(this.data,{
+	   		subject:'',
+	   		creator:'',
+	   		taskNum:'',
+	   		createTime:'',
+	   		classDesc:'',
+	   		classDetail:[]
+	   },true)
 	}, 
 	init:function () {
-		   
+		var url = location.href;
+		var classID = _.getParams(url).classID;
+		if(classID ==null || classID==''){
+			//不存在该课程
+		}else{
+			this.service.getClassDetail(classID,function (data,result) {
+				//成功函数
+				
+				this.data = data;
+				this.$update();
+			}.bind(this),function (data,result) {
+				//失败函数
+				
+			}.bind(this))
+		}
 	},  
 	enter:function(){
 	  	
