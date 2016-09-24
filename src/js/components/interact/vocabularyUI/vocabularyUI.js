6e8eb1e7ccd0d57b0ae25ed529af129aefa7aab9
chenbo <hzchenbo2014@corp.netease.com>
2016-09-24 15:39:59 +0800

    dev

--- src/js/components/interact/vocabularyUI/vocabularyUI.js
+++ src/js/components/interact/vocabularyUI/vocabularyUI.js
@@ -131,8 +131,8 @@ var VocabularyUI = BaseComponet.extend({
             for(var i=0 , _len = options.length; i<_len ;i++){
             	if(options[i].optionID === _optionID){
             		options[i].item_cont.zh = data.resInfo.zh;
-            		options[i].item_cont.sound.proTag = (data.resInfo.soundProTags||[])[0];
-            		options[i].item_cont.sound.id = data.resInfo.id;
+            		options[i].item_cont.sound.proTag = (data.resInfo.soundProTags||[])[0] || null;
+            		options[i].item_cont.sound.id = data.resInfo.id || null;
             		options[i].item_cont.id = data.resInfo.id;
             		this.$update();
             	}
